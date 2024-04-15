f2217c4698837c3c7b99d472db7d48e84d877edb
Leonid349 <159018167+Leonid349@users.noreply.github.com>
2024-04-15 14:38:14 +0300

    Update middle.h

--- middle.h
+++ middle.h
@@ -10,7 +10,7 @@ int itc_max_num(long long number);
 int itc_min_num(long long number);
 int itc_null_count(long long number);
 bool itc_mirror_num(long long number);
-
+int itc_mirror_count(long long number);
 
 
 #endif // MIDDLE_H_INCLUDED
